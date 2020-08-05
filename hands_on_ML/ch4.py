e863403af13b3dd2cc9611bc4a46ef1293d7b326
Rob Wells <robert.wells@colorado.edu>
2020-08-05 12:12:02 -0600

    Chapter 4 started. Laptop

---
+++ hands_on_ML/ch4.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+import sklearn.linear_model
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+
+#=============== Normal Equation and SVD Decomposition ======================#
+
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+
+X_b = np.c_[np.ones((100, 1)), X]  # add x0 = 1 to each instance
+theta_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
+
+X_new = np.array([[0], [2]])
+X_new_b = np.c_[np.ones((2, 1)), X_new] # add x0 = 1 to each instance
+y_predict = X_new_b.dot(theta_best)
+
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+print(lin_reg.intercept_, lin_reg.coef_)
+print(lin_reg.predict(X_new))
+
+theta_best_svd, residuals, rank, s = np.linalg.lstsq(X_b, y, rcond=1e-6)
+print(theta_best_svd)
+
+np.linalg.pinv(X_b).dot(y)
+
+
+#========================= Gradient Descent ===============================#
+
+#== Batch Gradient Descent ==#
+eta = 0.1  # learning rate
+n_iterations = 1000
+m = 100
+
+theta = np.random.randn(2,1)  # random initialization
+
+for iteration in range(n_iterations):
+    gradients = 2/m * X_b.T.dot(X_b.dot(theta) - y)
+    theta = theta - eta * gradients
+
+#== Stochastic Gradient Descent ==#
+n_epochs = 50
+t0, t1 = 5, 50  # learning schedule hyperparameters
+
+def learning_schedule(t):
+    return t0 / (t + t1)
+
+theta = np.random.randn(2,1)  # random initialization
+
+for epoch in range(n_epochs):
+    for i in range(m):
+        random_index = np.random.randint(m)
+        xi = X_b[random_index:random_index+1]
+        yi = y[random_index:random_index+1]
+        gradients = 2 * xi.T.dot(xi.dot(theta) - yi)
+        eta = learning_schedule(epoch * m + i)
+        theta = theta - eta * gradients
