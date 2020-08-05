ae97b0f08679f389e2d8a1790e1dd592feb159c9
Rob Wells <robert.wells@colorado.edu>
2020-08-04 22:02:58 -0600

    Chapter 3. Laptop

---
+++ hands_on_ML/ch3.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pandas as pd
+import sklearn.linear_model
+
+from sklearn.datasets import fetch_openml
+
+mnist = fetch_openml('mnist_784', version=1)
+
+X, y = mnist["data"], mnist["target"]
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+some_digit = X[0]
+some_digit_image = some_digit.reshape(28, 28)
+
+y = y.astype(np.uint8)
+
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+y_train_5 = (y_train == 5)  # True for all 5s, False for all other digits.
+y_test_5 = (y_test == 5)
+
+from sklearn.linear_model import SGDClassifier
+
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+
+print(sgd_clf.predict([some_digit]))
+
+#============================================================================#
+from sklearn.model_selection import StratifiedKFold
+from sklearn.base import clone
+
+skfolds = StratifiedKFold(n_splits=3, random_state=42)
+
+for train_index, test_index in skfolds.split(X_train, y_train_5):
+    clone_clf = clone(sgd_clf)
+    X_train_folds = X_train[train_index]
+    y_train_folds = y_train_5[train_index]
+    X_test_fold = X_train[test_index]
+    y_test_fold = y_train_5[test_index]
+
+    clone_clf.fit(X_train_folds, y_train_folds)
+    y_pred = clone_clf.predict(X_test_fold)
+    n_correct = sum(y_pred == y_test_fold)
+    print(n_correct / len(y_pred))  # prints 0.9502, 0.96565 and 0.96495
+#============================================================================#
+
+from sklearn.model_selection import cross_val_score
+cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy")
+
+from sklearn.model_selection import cross_val_predict
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+
+from sklearn.metrics import confusion_matrix
+confusion_matrix(y_train_5, y_train_pred)
+
+from sklearn.metrics import precision_score, recall_score
+precision_score(y_train_5, y_train_pred)
+recall_score(y_train_5, y_train_pred)
+
+from sklearn.metrics import f1_score
+f1_score(y_train_5, y_train_pred)
+
+y_scores = sgd_clf.decision_function([some_digit])
+threshold = 0
+y_some_digit_pred = (y_scores > threshold)
+
+y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3,
+                             method="decision_function")
+
+from sklearn.metrics import precision_recall_curve
+
+precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
+
+def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
+    plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
+    plt.plot(thresholds, recalls[:-1], "g-", label="Recall")
+    plt.xlabel("Threshold")
+    plt.legend(loc="center left")
+    plt.ylim([0, 1])
+
+plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
+plt.show()
