f2b995d32b9f6ae2faaae8c49367c92b2af30c1b
Rob Wells <robert.wells@colorado.edu>
2020-08-04 23:55:44 -0600

    Chapter 3 finished. Laptop.

--- hands_on_ML/ch3.py
+++ hands_on_ML/ch3.py
@@ -83,3 +83,41 @@ def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
 
 plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
 plt.show()
+
+
+from sklearn.metrics import roc_curve
+
+fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
+
+def plot_roc_curve(fpr, tpr, label=None):
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.axis([0, 1, 0, 1])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+
+plot_roc_curve(fpr, tpr)
+plt.show()
+
+from sklearn.metrics import roc_auc_score
+roc_auc_score(y_train_5, y_scores)
+
+from sklearn.ensemble import RandomForestClassifier
+
+
+forest_clf = RandomForestClassifier(random_state=42)
+y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3,
+                                    method="predict_proba")
+
+y_scores_forest = y_probas_forest[:, 1]   # score = proba of positive class
+fpr_forest, tpr_forest, thresholds_forest = roc_curve(y_train_5,y_scores_forest)
+
+
+from sklearn.neighbors import KNeighborsClassifier
+
+y_train_large = (y_train >= 7)
+y_train_odd = (y_train % 2 == 1)
+y_multilabel = np.c_[y_train_large, y_train_odd]
+
+knn_clf = KNeighborsClassifier()
+knn_clf.fit(X_train, y_multilabel)
