7bcd57ee9b143c59f16ace0faf4a944032b4a38b
robert-h-wells <robert.h.wells.iii@gmail.com>
2020-08-05 21:35:36 -0600

    Chapter 4 finished. Laptop

--- hands_on_ML/ch4.py
+++ hands_on_ML/ch4.py
@@ -58,3 +58,32 @@ for epoch in range(n_epochs):
         gradients = 2 * xi.T.dot(xi.dot(theta) - yi)
         eta = learning_schedule(epoch * m + i)
         theta = theta - eta * gradients
+
+from sklearn.linear_model import SGDRegressor
+sgd_reg = SGDRegressor(max_iter=50, penalty=None, eta0=0.1)
+sgd_reg.fit(X, y.ravel())
+
+
+from sklearn.base import clone
+
+# prepare the data
+poly_scaler = Pipeline([
+        ("poly_features", PolynomialFeatures(degree=90, include_bias=False)),
+        ("std_scaler", StandardScaler()) ])
+X_train_poly_scaled = poly_scaler.fit_transform(X_train)
+X_val_poly_scaled = poly_scaler.transform(X_val)
+
+sgd_reg = SGDRegressor(max_iter=1, tol=-np.infty, warm_start=True,
+                       penalty=None, learning_rate="constant", eta0=0.0005)
+
+minimum_val_error = float("inf")
+best_epoch = None
+best_model = None
+for epoch in range(1000):
+    sgd_reg.fit(X_train_poly_scaled, y_train)  # continues where it left off
+    y_val_predict = sgd_reg.predict(X_val_poly_scaled)
+    val_error = mean_squared_error(y_val, y_val_predict)
+    if val_error < minimum_val_error:
+        minimum_val_error = val_error
+        best_epoch = epoch
+        best_model = clone(sgd_reg)
