1f0e56b42aa02acb2c04731aad1ef36e098da527
Rob Wells <vaandei421@gmail.com>
2020-08-06 09:48:48 -0600

    Chapter 5. Desktop

---
+++ hands_on_ML/ch5.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+import sklearn.linear_model
+import matplotlib.pyplot as plt
+
+from sklearn import datasets
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+
+iris = datasets.load_iris()
+X = iris["data"][:, (2, 3)]  # petal length, petal width
+y = (iris["target"] == 2).astype(np.float64)  # Iris-Virginica
+
+svm_clf = Pipeline([
+        ("scaler", StandardScaler()),
+        ("linear_svc", LinearSVC(C=1, loss="hinge")),
+    ])
+
+svm_clf.fit(X, y)
+
+
+from sklearn.datasets import make_moons
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+
+polynomial_svm_clf = Pipeline([
+        ("poly_features", PolynomialFeatures(degree=3)),
+        ("scaler", StandardScaler()),
+        ("svm_clf", LinearSVC(C=10, loss="hinge"))
+    ])
+
+polynomial_svm_clf.fit(X, y)
+
+
+from sklearn.svm import SVC
+poly_kernel_svm_clf = Pipeline([
+        ("scaler", StandardScaler()),
+        ("svm_clf", SVC(kernel="poly", degree=3, coef0=1, C=5))
+    ])
+poly_kernel_svm_clf.fit(X, y)
+
+
+rbf_kernel_svm_clf = Pipeline([
+        ("scaler", StandardScaler()),
+        ("svm_clf", SVC(kernel="rbf", gamma=5, C=0.001))
+    ])
+rbf_kernel_svm_clf.fit(X, y)
+
+#============== SVM Regression ==================#
+from sklearn.svm import LinearSVR
+
+svm_reg = LinearSVR(epsilon=1.5)
+svm_reg.fit(X, y)
+
+from sklearn.svm import SVR
+
+svm_poly_reg = SVR(kernel="poly", degree=2, C=100, epsilon=0.1)
+svm_poly_reg.fit(X, y)
