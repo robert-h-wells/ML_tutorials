3d5332d610417076a68088019859bb4da29730b8
robert-h-wells <robert.h.wells.iii@gmail.com>
2020-08-07 11:50:29 -0600

    Chapter 8 finished. Laptop

---
+++ hands_on_ML/ch8.py
@@ -0,0 +1,28 @@
+from sklearn.decomposition import PCA
+
+pca = PCA(n_components = 2)
+X2D = pca.fit_transform(X)
+
+
+from sklearn.decomposition import KernelPCA
+
+rbf_pca = KernelPCA(n_components = 2, kernel="rbf", gamma=0.04)
+X_reduced = rbf_pca.fit_transform(X)
+
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+
+clf = Pipeline([
+        ("kpca", KernelPCA(n_components=2)),
+        ("log_reg", LogisticRegression())
+    ])
+
+param_grid = [{
+        "kpca__gamma": np.linspace(0.03, 0.05, 10),
+        "kpca__kernel": ["rbf", "sigmoid"]
+    }]
+
+grid_search = GridSearchCV(clf, param_grid, cv=3)
+grid_search.fit(X, y)
