cf2ea1ac1e7acb1ef30858318ea37d71e92bc914
robert-h-wells <robert.h.wells.iii@gmail.com>
2020-08-07 17:02:57 -0600

    Added code halfway through Ch. 4. Laptop

--- hands_on_ML/useful.py
+++ hands_on_ML/useful.py
@@ -72,6 +72,15 @@ scores = cross_val_score(model, data_prepared, data_labels,
                          scoring="neg_mean_squared_error", cv=10)
 tree_rmse_scores = np.sqrt(-scores)
 
+
+# Polynomial Regression
+from sklearn.preprocessing import PolynomialFeatures
+poly_features = PolynomialFeatures(degree=2, include_bias=False)
+X_poly = poly_features.fit_transform(X)
+lin_reg = LinearRegression()
+lin_reg.fit(X_poly, y)
+lin_reg.intercept_, lin_reg.coef_
+
 #======================================================================================#
 # Grid search to find the best hyperparameters 
 # This example uses the RandomForestRegressor
@@ -94,3 +103,56 @@ grid_search.best_estimator_
 
 # Use Randomized when the number of parameters is very big
 from sklearn.model_selection import RandomizedSearchCV
+
+#======================================================================================#
+# Classifiers
+
+# Stochastic Gradient Descent Classifier
+from sklearn.linear_model import SGDClassifier
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+
+# Confusion Matrix
+from sklearn.model_selection import cross_val_predict
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+from sklearn.metrics import confusion_matrix
+confusion_matrix(y_train_5, y_train_pred)
+# True Negative  | False Positive     (said it was negative and it is | said it was positive and it's not)
+# False Negative | True Positive      (said it was negative and it's not | said it was positive and it is)
+from sklearn.metrics import precision_score, recall_score
+# precision = TP / (TP + FP)
+# recall    = TP / (TP + FN) 
+
+# F1 score combines precision and recall 
+from sklearn.metrics import f1_score
+
+# Curve that shows how precision and recall are connected
+from sklearn.metrics import precision_recall_curve
+precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
+
+# Similar to precision recall curve, receiver operating characteristic
+from sklearn.metrics import roc_curve
+fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
+
+# Random Forest Classifier
+from sklearn.ensemble import RandomForestClassifier
+forest_clf = RandomForestClassifier(random_state=42)
+y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3,
+                                    method="predict_proba")
+
+
+# Multilabel Classifiers
+from sklearn.multiclass import OneVsOneClassifier
+ovo_clf = OneVsOneClassifier(SGDClassifier(random_state=42))
+ovo_clf.fit(X_train, y_train)
+ovo_clf.predict([some_digit])
+
+# True Multilabel
+from sklearn.neighbors import KNeighborsClassifier
+y_train_large = (y_train >= 7)
+y_train_odd = (y_train % 2 == 1)
+y_multilabel = np.c_[y_train_large, y_train_odd]
+knn_clf = KNeighborsClassifier()
+knn_clf.fit(X_train, y_multilabel)
+
+#======================================================================================#
