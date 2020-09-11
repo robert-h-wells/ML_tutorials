32019c51fe864116e105c388bf0c9e6e8f442a9b
Rob Wells <vaandei421@gmail.com>
2020-09-10 20:27:05 -0600

    Unsupervised problems. 1 left.

---
+++ hands_on_ML/ch9_problems.py
@@ -0,0 +1,182 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import fetch_openml
+from sklearn.metrics import accuracy_score
+
+from sklearn.datasets import fetch_olivetti_faces
+
+olivetti = fetch_olivetti_faces()
+
+# Problem 10
+
+# Use Stratified since dataset is small
+from sklearn.model_selection import StratifiedShuffleSplit
+
+strat_split = StratifiedShuffleSplit(n_splits=1, test_size=40, random_state=42)
+
+train_valid_idx, test_idx = next(strat_split.split(olivetti.data, olivetti.target))
+X_train_valid = olivetti.data[train_valid_idx]
+y_train_valid = olivetti.target[train_valid_idx]
+X_test = olivetti.data[test_idx]
+y_test = olivetti.target[test_idx]
+
+strat_split = StratifiedShuffleSplit(n_splits=1, test_size=80, random_state=43)
+train_idx, valid_idx = next(strat_split.split(X_train_valid, y_train_valid))
+X_train = X_train_valid[train_idx]
+y_train = y_train_valid[train_idx]
+X_valid = X_train_valid[valid_idx]
+y_valid = y_train_valid[valid_idx]
+
+print(np.shape(X_train),np.shape(X_valid),np.shape(X_test))
+
+# Reduce data dimension to speed things up
+from sklearn.decomposition import PCA
+
+pca = PCA(0.99)
+X_train_pca = pca.fit_transform(X_train)
+X_valid_pca = pca.transform(X_valid)
+X_test_pca = pca.transform(X_test)
+
+print(pca.n_components_)
+
+# Determine a good number of clusters
+from sklearn.cluster import KMeans
+
+k_range = range(5, 150, 5)
+kmeans_per_k = []
+for k in k_range:
+    print("k={}".format(k))
+    kmeans = KMeans(n_clusters=k, random_state=42).fit(X_train_pca)
+    kmeans_per_k.append(kmeans)
+
+# Look at silhouette score to determine best cluster number
+from sklearn.metrics import silhouette_score
+
+silhouette_scores = [silhouette_score(X_train_pca, model.labels_)
+                     for model in kmeans_per_k]
+best_index = np.argmax(silhouette_scores)
+best_k = k_range[best_index]
+best_score = silhouette_scores[best_index]
+
+plt.figure(figsize=(8, 3))
+plt.plot(k_range, silhouette_scores, "bo-")
+plt.xlabel("$k$", fontsize=14)
+plt.ylabel("Silhouette score", fontsize=14)
+plt.plot(best_k, best_score, "rs")
+plt.show()
+
+# Intertia isn't as useful as silhouette score but will show the code
+inertias = [model.inertia_ for model in kmeans_per_k]
+best_inertia = inertias[best_index]
+
+plt.figure(figsize=(8, 3.5))
+plt.plot(k_range, inertias, "bo-")
+plt.xlabel("$k$", fontsize=14)
+plt.ylabel("Inertia", fontsize=14)
+plt.plot(best_k, best_inertia, "rs")
+plt.show()
+
+best_model = kmeans_per_k[best_index]
+
+# Show the model predictions
+def plot_faces(faces, labels, n_cols=5):
+    n_rows = (len(faces) - 1) // n_cols + 1
+    plt.figure(figsize=(n_cols, n_rows * 1.1))
+    for index, (face, label) in enumerate(zip(faces, labels)):
+        plt.subplot(n_rows, n_cols, index + 1)
+        plt.imshow(face.reshape(64, 64), cmap="gray")
+        plt.axis("off")
+        plt.title(label)
+    plt.show()
+
+for cluster_id in np.unique(best_model.labels_):
+    print("Cluster", cluster_id)
+    in_cluster = best_model.labels_==cluster_id
+    faces = X_train[in_cluster].reshape(-1, 64, 64)
+    labels = y_train[in_cluster]
+    plot_faces(faces, labels)
+
+
+# Problem 11
+# test model with Classifier
+from sklearn.ensemble import RandomForestClassifier
+
+rnd_clf = RandomForestClassifier(n_estimators=150, random_state=42)
+rnd_clf.fit(X_train_pca, y_train)
+
+y_pred_rf = rnd_clf.predict(X_valid_pca)
+accuracy_score(y_valid, y_pred_rf)
+
+# See accuracy when using the "best model"
+X_train_reduced = best_model.transform(X_train_pca)
+X_valid_reduced = best_model.transform(X_valid_pca)
+X_test_reduced = best_model.transform(X_test_pca)
+
+clf = RandomForestClassifier(n_estimators=150, random_state=42)
+clf.fit(X_train_reduced, y_train)
+    
+clf.score(X_valid_reduced, y_valid)
+
+
+from sklearn.pipeline import Pipeline
+
+for n_clusters in k_range:
+    pipeline = Pipeline([
+        ("kmeans", KMeans(n_clusters=n_clusters, random_state=n_clusters)),
+        ("forest_clf", RandomForestClassifier(n_estimators=150, random_state=42))
+    ])
+    pipeline.fit(X_train_pca, y_train)
+    print(n_clusters, pipeline.score(X_valid_pca, y_valid))
+
+
+# Look at accuracy without PCA
+from sklearn.pipeline import Pipeline
+
+for n_clusters in k_range:
+    pipeline = Pipeline([
+        ("kmeans", KMeans(n_clusters=n_clusters, random_state=n_clusters)),
+        ("forest_clf", RandomForestClassifier(n_estimators=150, random_state=42))
+    ])
+    pipeline.fit(X_train, y_train)
+    print(n_clusters, pipeline.score(X_valid, y_valid))
+
+
+# Problem 12
+from sklearn.mixture import GaussianMixture
+
+gm = GaussianMixture(n_components=40, random_state=42)
+y_pred = gm.fit_predict(X_train_pca)
+
+# Generate new faces
+n_gen_faces = 20
+gen_faces_reduced, y_gen_faces = gm.sample(n_samples=n_gen_faces)
+gen_faces = pca.inverse_transform(gen_faces_reduced)
+
+plot_faces(gen_faces, y_gen_faces)
+
+# Modify images
+n_rotated = 4
+rotated = np.transpose(X_train[:n_rotated].reshape(-1, 64, 64), axes=[0, 2, 1])
+rotated = rotated.reshape(-1, 64*64)
+y_rotated = y_train[:n_rotated]
+
+n_flipped = 3
+flipped = X_train[:n_flipped].reshape(-1, 64, 64)[:, ::-1]
+flipped = flipped.reshape(-1, 64*64)
+y_flipped = y_train[:n_flipped]
+
+n_darkened = 3
+darkened = X_train[:n_darkened].copy()
+darkened[:, 1:-1] *= 0.3
+darkened = darkened.reshape(-1, 64*64)
+y_darkened = y_train[:n_darkened]
+
+X_bad_faces = np.r_[rotated, flipped, darkened]
+y_bad = np.concatenate([y_rotated, y_flipped, y_darkened])
+
+plot_faces(X_bad_faces, y_bad)
+
+
+# Problem 13
